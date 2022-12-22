2667725470b19f3c7bdcc78e46509b97b52f94be
Igwegbesamson <35536836+Igwegbesamson@users.noreply.github.com>
2022-12-23 00:54:53 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,17 @@
-import Building from './5-building.js';
+export default class Building {
+  constructor(sqft) {
+    if (
+      new.target !== Building && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this._sqft = sqft;
+  }
 
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-  new TestBuilding(200)
-}
-catch(err) {
-  console.log(err);
+  // sqft getter
+  get sqft() {
+    return this._sqft;
+  }
 }
