8ecb21f6c71ec45b2cbee5b1ca1b4a32a2014155
Igwegbesamson <35536836+Igwegbesamson@users.noreply.github.com>
2023-01-04 05:32:46 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,3 @@
-const getStudentsByLocation = (students, city) => {
-  const arratyObjec = students.filter((funtion) => funtion.location === city);
-  return arratyObjec;
-};
-
-export default getStudentsByLocation
+export default function getStudentsByLocation(lst, city) {
+  return lst.filter((obj) => obj.location === city);
+}
